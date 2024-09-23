2af5c856b8e4ddd88f3327ecc15168cd0f88609b
janbaangkn <jan.baan@fokker.com>
2024-09-23 16:17:00 +0200

    added product locations

--- penelope_fokker_module/TCPIP_test.py
+++ penelope_fokker_module/TCPIP_test.py
@@ -13,8 +13,7 @@ tcp_client_thread.start()
 message = "get_status"
 
 feedback = send_message(uid=robot_uid, message=message, feedback=True)
-if feedback:
-    print(f"Feedback: {feedback}")
+print(f"Feedback: {feedback}")
 
 # add hole locations, stack thickness and diameter in the permanent fastener storage list 
 msg = "populate_agent<"\
@@ -196,7 +195,147 @@ feedback = send_message(uid=robot_uid, message=msg, feedback=True)
 if feedback:
     print(f"Feedback: {feedback}")
 
-# add temporary fastener in storage
+
+# add hole locations, stack thickness and diameter in the product list
+msg = "populate_agent<"\
+"product<"\
+    "uid<product>"\
+    "locations<"\
+        "hole_location<"\
+            "uid<left_drill_jig_01>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<11.0>"\
+                "pose_p_y<838.0>"\
+                "pose_p_z<1117.5>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<75.0>"\
+                "pose_o_z<0.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<left_drill_jig_08>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<13.5>"\
+                "pose_p_y<902.0>"\
+                "pose_p_z<908.5>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<69.0>"\
+                "pose_o_z<0.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<left_drill_jig_15>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<16.5>"\
+                "pose_p_y<991.0>"\
+                "pose_p_z<708.0>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<63.0>"\
+                "pose_o_z<0.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<inner_vert_jig_01_01>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<222.0>"\
+                "pose_p_y<865.0>"\
+                "pose_p_z<1021.5>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<72.0>"\
+                "pose_o_z<10.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<inner_vert_jig_01_08>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<226.0>"\
+                "pose_p_y<938.0>"\
+                "pose_p_z<815.0>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<66.0>"\
+                "pose_o_z<10.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<inner_vert_jig_02_01>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<450.8>"\
+                "pose_p_y<865.0>"\
+                "pose_p_z<1020.0>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<72.0>"\
+                "pose_o_z<25.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<inner_vert_jig_02_08>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<452.0>"\
+                "pose_p_y<938.0>"\
+                "pose_p_z<814.0>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<66.0>"\
+                "pose_o_z<25.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<inner_vert_jig_03_01>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<621.0>"\
+                "pose_p_y<862.0>"\
+                "pose_p_z<1021.5>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<72.0>"\
+                "pose_o_z<30.0>"\
+            ">"\
+        ">"\
+        "hole_location<"\
+            "uid<inner_vert_jig_03_08>"\
+            "max_obstacle_height<40.0>"\
+            "diam<5.0>"\
+            "stack_thickness_tag<9.0>"\
+            "pose<"\
+                "pose_p_x<623.0>"\
+                "pose_p_y<938.0>"\
+                "pose_p_z<817.0>"\
+                "pose_o_x<90.0>"\
+                "pose_o_y<66.0>"\
+                "pose_o_z<30.0>"\
+            ">"\
+        ">"\
+    ">"\
+">"
+
+feedback = send_message(uid=robot_uid, message=msg, feedback=True)
+if feedback:
+    print(f"Feedback: {feedback}")
+
+# add 3 temporary fasteners in storage
+# add 8 temporary fasteners in the product
 msg = "populate_agent<"\
 "tempfs<"\
     "tempf<"\
@@ -234,7 +373,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_04>"\
-        "loc_uid<tfst_1_4>"\
+        "loc_uid<left_drill_jig_08>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -245,7 +384,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_05>"\
-        "loc_uid<tfst_2_1>"\
+        "loc_uid<left_drill_jig_15>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -256,7 +395,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_06>"\
-        "loc_uid<tfst_2_2>"\
+        "loc_uid<inner_vert_jig_01_01>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -267,7 +406,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_07>"\
-        "loc_uid<tfst_2_3>"\
+        "loc_uid<inner_vert_jig_01_08>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -278,7 +417,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_08>"\
-        "loc_uid<tfst_2_4>"\
+        "loc_uid<inner_vert_jig_02_01>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -289,7 +428,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_09>"\
-        "loc_uid<tfst_3_1>"\
+        "loc_uid<inner_vert_jig_02_08>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -300,7 +439,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_10>"\
-        "loc_uid<tfst_3_2>"\
+        "loc_uid<inner_vert_jig_03_01>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -311,7 +450,7 @@ msg = "populate_agent<"\
     ">"\
     "tempf<"\
         "uid<tempf_11>"\
-        "loc_uid<tfst_3_3>"\
+        "loc_uid<inner_vert_jig_03_08>"\
         "fastener_state<1>"\
         "diam<5.0>"\
         "shaft_height<60.0>"\
@@ -326,61 +465,17 @@ feedback = send_message(uid=robot_uid, message=msg, feedback=True)
 if feedback:
     print(f"Feedback: {feedback}")
 
-# # add hole locations, stack thickness and diameter in the product list
-msg = "populate_agent<"\
-"product<"\
-    "uid<product>"\
-    "locations<"\
-        "hole_location<"\
-            "uid<left_drill_jig_01>"\
-            "max_obstacle_height<40.0>"\
-            "diam<5.0>"\
-            "stack_thickness_tag<9.0>"\
-            "pose<"\
-                "pose_p_x<11.0>"\
-                "pose_p_y<794.0>"\
-                "pose_p_z<1105.0>"\
-                "pose_o_x<90.0>"\
-                "pose_o_y<75.0>"\
-                "pose_o_z<0.0>"\
-            ">"\
-        ">"\
-        "hole_location<"\
-            "uid<left_drill_jig_08>"\
-            "max_obstacle_height<40.0>"\
-            "diam<5.0>"\
-            "stack_thickness_tag<9.0>"\
-            "pose<"\
-                "pose_p_x<13.5>"\
-                "pose_p_y<859.0>"\
-                "pose_p_z<892.0>"\
-                "pose_o_x<90.0>"\
-                "pose_o_y<69.0>"\
-                "pose_o_z<0.0>"\
-            ">"\
-        ">"\
-        "hole_location<"\
-            "uid<left_drill_jig_15>"\
-            "max_obstacle_height<40.0>"\
-            "diam<5.0>"\
-            "stack_thickness_tag<9.0>"\
-            "pose<"\
-                "pose_p_x<16.5>"\
-                "pose_p_y<949.0>"\
-                "pose_p_z<687.0>"\
-                "pose_o_x<90.0>"\
-                "pose_o_y<63.0>"\
-                "pose_o_z<0.0>"\
-            ">"\
-        ">"\
-    ">"\
-">"
-
-feedback = send_message(uid=robot_uid, message=msg, feedback=True)
-if feedback:
-    print(f"Feedback: {feedback}")
-
-# add two actions
+# add ten actions
+# 01: install a tempf in left_drill_jig_01
+# 02: remove tempf from left_drill_jig_01
+# 03: remove tempf from left_drill_jig_08
+# 04: remove tempf from left_drill_jig_15
+# 05: remove tempf from inner_vert_jig_01_01
+# 06: remove tempf from inner_vert_jig_01_08
+# 07: remove tempf from inner_vert_jig_02_01
+# 08: remove tempf from inner_vert_jig_02_08
+# 09: remove tempf from inner_vert_jig_03_01
+# 10: remove tempf from inner_vert_jig_03_08
 msg = "populate_agent<"\
 "actions<"\
     "action<"\
@@ -393,14 +488,63 @@ msg = "populate_agent<"\
     "action<"\
         "uid<A02>"\
         "action_type<remove_fastener>"\
+        "loc_uid<left_drill_jig_01>"\
+        "action_state<1>"\
+        "speed<100>"\
+    ">"\
+    "action<"\
+        "uid<A03>"\
+        "action_type<remove_fastener>"\
         "loc_uid<left_drill_jig_08>"\
         "action_state<1>"\
         "speed<100>"\
     ">"\
-        "action<"\
-        "uid<A03>"\
+    "action<"\
+        "uid<A04>"\
         "action_type<remove_fastener>"\
         "loc_uid<left_drill_jig_15>"\
+        "action_state<1>"\
+        "speed<100>"\
+    ">"\
+    "action<"\
+        "uid<A05>"\
+        "action_type<remove_fastener>"\
+        "loc_uid<inner_vert_jig_01_01>"\
+        "action_state<1>"\
+        "speed<100>"\
+    ">"\
+    "action<"\
+        "uid<A06>"\
+        "action_type<remove_fastener>"\
+        "loc_uid<inner_vert_jig_01_02>"\
+        "action_state<1>"\
+        "speed<100>"\
+    ">"\
+    "action<"\
+        "uid<A07>"\
+        "action_type<remove_fastener>"\
+        "loc_uid<inner_vert_jig_02_01>"\
+        "action_state<1>"\
+        "speed<100>"\
+    ">"\
+    "action<"\
+        "uid<A08>"\
+        "action_type<remove_fastener>"\
+        "loc_uid<inner_vert_jig_02_02>"\
+        "action_state<1>"\
+        "speed<100>"\
+    ">"\
+    "action<"\
+        "uid<A09>"\
+        "action_type<remove_fastener>"\
+        "loc_uid<inner_vert_jig_03_01>"\
+        "action_state<1>"\
+        "speed<100>"\
+    ">"\
+    "action<"\
+        "uid<A10>"\
+        "action_type<remove_fastener>"\
+        "loc_uid<inner_vert_jig_03_02>"\
         "action_state<1>"\
         "speed<100>"\
     ">"\
